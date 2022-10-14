b4d87394a67ad9adc8a636947057ae1611129c55
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-14 16:08:38 +0200

    [correzione] dicegame

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,30 +1,19 @@
-play.addEventListener('click', function(){
-  const numeri = [];
-  const nRandom1 = Math.floor(Math.random() * 7);
-  const nRandom2 = Math.floor(Math.random() * 7);
-  numeri.push(nRandom1,nRandom2);
-  const nUser = numeri[0];
-  const nPc = numeri[1];
+/*DICE*/
 
-  if(nUser > nPc){
-  document.getElementById('nuser').innerHTML = numeri[0];
-  document.getElementById('npc').innerHTML = numeri[1];
+play.addEventListener('click', function(){
+  
+  const user = Math.floor(Math.random() * 7);
+  const cpu  = Math.floor(Math.random() * 7);
+  document.getElementById('nuser').innerHTML = user;
+  document.getElementById('npc').innerHTML = cpu;
+
+  if(user > cpu){
+
   document.getElementById('winner').innerHTML = 'HAI VINTO!';
-  }else if(nUser === nPc){
-  document.getElementById('nuser').innerHTML = numeri[0];
-  document.getElementById('npc').innerHTML = numeri[1];
+  }else if(user === cpu){
   document.getElementById('winner').innerHTML = 'PARI!';
   }else{
-  document.getElementById('nuser').innerHTML = numeri[0];
-  document.getElementById('npc').innerHTML = numeri[1];
   document.getElementById('winner').innerHTML = 'HAI PERSO!';
   }
-  console.log(numeri);
+  
 });
-
-
-
-
-
-
-
