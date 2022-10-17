4269a6b5ad95b43ea4cf193c4f40988ef281e3ff
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-17 09:53:09 +0200

    [correzione] mail

--- jsmail/js/script.js
+++ jsmail/js/script.js
@@ -8,9 +8,9 @@ let isUsermail = false;
 const list = ["franco@gmail.com","gennaro@gmail.com","laura@gmail.com","riccardo@gmail.com"]
 
 submit.addEventListener('click', function(){
+  
   const userEmail = email.value
   for(let i = 0; i < list.length; i++){
-    console.log(list[i]);
     if(list[i] === userEmail){
       isUsermail = true;
     }
@@ -19,11 +19,10 @@ submit.addEventListener('click', function(){
     output.innerHTML = `
     <i class="bi bi-check-square"></i> La tua mail è presente nel sistema, puoi accedere!
     `;
-    add.classList.add('d-none');
   }else{
     output.innerHTML = `
     <i class="bi bi-exclamation-triangle"></i> La tua mail NON è presente nel sistema, NON puoi accedere!
     `;
   }
+  isUsermail = false;
 });
-
